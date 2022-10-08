cb9aeb6e21286443903e2ef334d8e84181193484
dmitrysv007 <77918171+dmitrysv007@users.noreply.github.com>
2022-10-08 09:20:29 +0400

    Initial commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = {
+  title: 'Vue3 Audio Player',
+  description: 'This is a beautiful Audio Player Component for Vue3.',
+  // base: '/vue3-audio-player/',
+  themeConfig: {
+    repo: 'https://github.com/RealCoolSnow/vue3-audio-player',
+    sidebar: [
+      {
+        text: 'Introduction',
+        children: [{ text: 'Getting Started', link: '/' }],
+      },
+    ],
+  },
+  vite: {
+    resolve: {
+      alias: {
+        'vue3-audio-player': path.resolve(__dirname, '../../src'),
+      },
+      dedupe: ['vue', /prime\/.+/], // avoid error when using dependencies that also use Vue
+    },
+  },
+}
